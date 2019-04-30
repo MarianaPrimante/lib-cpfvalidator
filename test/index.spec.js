ec4009a443af842ab33102a8dbd5937bcde5ce9f
Mariana <maprimante@gmail.com>
2019-04-30 15:09:40 -0300

    npm publish

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,28 +2,34 @@ const lib = require("../lib/index");
 const expect = require("chai").expect;
 
 describe("cpfValidator", () => {
-  it("valid", () => {
+  it("valid : '38748141810'", () => {
     expect(lib.cpfValidator("38748141810")).to.be.equal(true);
   });
-  it("valid", () => {
+  it("valid: 38748141810", () => {
+    expect(lib.cpfValidator(38748141810)).to.be.equal(true);
+  });
+  it("valid: '40090279832'", () => {
     expect(lib.cpfValidator("40090279832")).to.be.equal(true);
   });
-  it("invalid", () => {
+  it("invalid: '1111111111'", () => {
     expect(lib.cpfValidator("11111111111")).to.be.equal(false);
   });
-  it("invalid", () => {
+  it("invalid: 11111111111", () => {
+    expect(lib.cpfValidator(11111111111)).to.be.equal(false);
+  });
+  it("invalid: '88888888888'", () => {
     expect(lib.cpfValidator("88888888888")).to.be.equal(false);
   });
-  it("invalid", () => {
+  it("invalid: '27727282822'", () => {
     expect(lib.cpfValidator("27727282822")).to.be.equal(false);
   });
-  it("invalid", () => {
+  it("invalid: 'aaaaaaaaaaa'", () => {
     expect(lib.cpfValidator("aaaaaaaaaaa")).to.be.equal(false);
   });
-  it("invalid", () => {
+  it("invalid: '||\\$$$57&'", () => {
     expect(lib.cpfValidator("||\\$$$57&")).to.be.equal(false);
   });
-  it("invalid", () => {
+  it("invalid: 'a5676a6a7'", () => {
     expect(lib.cpfValidator("a5676a6a7")).to.be.equal(false);
   });
 });
